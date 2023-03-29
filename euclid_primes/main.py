c1f4d80094e6b1d419613a05f789fd78da54e4d9
zergultral <zergultral@gmail.com>
2023-03-29 11:11:05 +0300

    Modify Euclid primes

--- euclid_primes/main.py
+++ euclid_primes/main.py
@@ -2,14 +2,14 @@
 # https://youtu.be/f5slLeCz7p8
 
 
-n = 107 * 199  # int(input())
+n = 997 * 3571  # int(input())
 print(n)
 
 basis = 8
 r = 1
 while (basis ** r) % n != 1:
     r += 1
-    print(r, basis ** r, basis ** r // n, basis ** r % n)
+    # print(r, basis ** r, basis ** r // n, basis ** r % n)
 
 pred_euklid_multiplier = int(basis ** (r // 2) - 1)
 numerator = pred_euklid_multiplier
@@ -29,5 +29,6 @@ multiplier2 = n // multiplier1
 
 print(pred_euklid_multiplier % 13)
 print(pred_euklid_multiplier % 23)
+print(r)
 print(multiplier1)
 print(multiplier2)
