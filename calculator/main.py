fbb7edaa95aa68c220a4980419afd1453a22024a
zergultral <zergultral@gmail.com>
2023-04-05 10:03:52 +0300

    Implement negative numbers

--- calculator/main.py
+++ calculator/main.py
@@ -1,17 +1,17 @@
-jopa = '10 - 2'
+jopa = '-10 - 2'
 
 
 def simple_operation(example):
     example = example.replace(' ', '')
-    for i in range(0, len(example)):
+    for i in range(1, len(example)):
         if example[i] in '+-/*':
             begin1 = i - 1
             end1 = i - 1
-            while begin1 > 0 and example[begin1 - 1] in '0123456789':
+            while begin1 > 0 and example[begin1 - 1] in '-0123456789':
                 begin1 -= 1
             begin2 = i + 1
             end2 = i + 1
-            while end2 < len(example) - 1 and example[end2 + 1] in '0123456789':
+            while end2 < len(example) - 1 and example[end2 + 1] in '-0123456789':
                 end2 += 1
             number1 = int(example[begin1:end1 + 1])
             number2 = int(example[begin2:end2 + 1])
