24a4da08e7415400c29ffe4c268e9624c31c1408
zergultral <zergultral@gmail.com>
2023-06-05 17:40:07 +0300

    Implement Jump game

--- calculator/main.py
+++ calculator/main.py
@@ -7,24 +7,6 @@
 #     elif jopa[i] != ' ':
 #         jopa2 += jopa[i]
 
-def simple_operation(example):
-    for i in range(1, len(example)):
-        if example[i] in '+-*/':
-            operator = example[i]
-            break
-    operand1 = float(example[:i].replace('_', '-'))
-    operand2 = float(example[i + 1:].replace('_', '-'))
-    if operator == '+':
-        result = operand1 + operand2
-    elif operator == '-':
-        result = operand1 - operand2
-    elif operator == '*':
-        result = operand1 * operand2
-    elif operator == '/':
-        result = operand1 / operand2
-    return result
-
-
 # if example[i] in '+-/*':
 #     begin1 = i - 1
 #     end1 = i - 1
@@ -47,17 +29,67 @@ def simple_operation(example):
 #     return result
 
 
+def simple_operation(example):
+    for i in range(1, len(example)):
+        if example[i] in '+-*/':
+            operator = example[i]
+            break
+    operand1 = float(example[:i].replace('_', '-'))
+    operand2 = float(example[i + 1:].replace('_', '-'))
+    if operator == '+':
+        result = operand1 + operand2
+    elif operator == '-':
+        result = operand1 - operand2
+    elif operator == '*':
+        result = operand1 * operand2
+    elif operator == '/':
+        result = operand1 / operand2
+    return result
+
+
+def skobki(example):
+    i = 0
+    while i < len(example):
+        if example[i] == '(':
+            example = example[:i] + skobki(example[i + 1:])
+        elif example[i] == ')':
+            example = str(main(example[:i])) + example[i + 1:]
+        i += 1
+    return example
+
+
 def main(example):
+    print(example)
     example = example.replace(' ', '')
+    i = 0
+    while i < len(example):
+        if example[i] == '(':
+            example = example[:i] + skobki(example[i + 1:])
+        i += 1
     if not ('+' in example or '-' in example or '*' in example or '/' in example):
         return example
-    for i in range(1, len(example)):
-        if example[i] in '+-/*':
-            end = i + 1
-            while end < len(example) - 1 and example[end + 1] in '_.0123456789':
-                end += 1
-            break
-    return float(main(str(simple_operation(example[:end + 1])) + example[end + 1:]))
+    if '*' in example or '/' in example:
+        for i in range(1, len(example)):
+            if example[i] in '*/':
+                end = i + 1
+                while end < len(example) - 1 and example[end + 1] in '_.0123456789':
+                    end += 1
+                start = i - 1
+                while start > 0 and example[start - 1] in '_.0123456789':
+                    start -= 1
+                break
+    else:
+        for i in range(1, len(example)):
+            if example[i] in '+-':
+                end = i + 1
+                while end < len(example) - 1 and example[end + 1] in '_.0123456789':
+                    end += 1
+                start = i - 1
+                while start > 0 and example[start - 1] in '_.0123456789':
+                    start -= 1
+                break
+    return float(main(example[:start] + str(simple_operation(example[start:end + 1])) + example[end + 1:]))
+
 
 # if end < len(example) - 1:
 #     return main(str(simple_operation(operation)) + example[end + 1:])
@@ -66,12 +98,17 @@ def main(example):
 
 
 test = [
-    ['1+1', 2],
-    ['2*2*3', 12],
-    ['2*2+2', 6],
-    ['1000/100', 10],
-    ['0.1/0.5', 0.2],
-    ['10/5+1*4/12', 1]
+    # ['1+1', 2],
+    # ['(2*2)*3', 12],
+    # ['2*2+2', 6],
+    # ['2+2*2', 6],
+    # ['(2+2)*2', 8],
+    # ['(2+2)*2+(2+2)*2', 16],
+    ['(((2+2)*2)+((2+2)*2))', 16],
+    # ['1000/100', 10],
+    # ['0.1/0.5', 0.2],
+    # ['10/5+12/4', 5],
+    # ['2*5-3*3', 1]
 ]
 
 error = False
